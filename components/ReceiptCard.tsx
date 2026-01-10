828282b33d5fe4865ca40b56aef0320dd90c3008
nadrimarket <avirex822@gmail.com>
2026-01-10 22:19:08 +0900

    Update ReceiptCard.tsx

--- components/ReceiptCard.tsx
+++ components/ReceiptCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import type { Receipt } from "@/lib/types";
 
 export function ReceiptCard({ receipt, notice }: { receipt: Receipt; notice: string }) {
   return (
